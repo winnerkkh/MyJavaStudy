1980e387e1380de5c76a8fdb46dfacf1615625a5
winnerkkh <winnerkkh@gmail.com>
2017-11-15 17:29:26 +0900

    updated

--- Java_Test/src/test_171115/GoodsExample.java
+++ Java_Test/src/test_171115/GoodsExample.java
@@ -18,27 +18,36 @@ public class GoodsExample {
 		Scanner scanner = new Scanner(System.in);
 
 		Goods goods = new Goods("새우깡", 1000);
+		
+		//null값으로 초기화해야함
+		//추후 값 입력은 사용자로부터 입력받을 예정
 		LocalDate startDate = null;
-		LocalDate eventDate = null;
+		LocalDate eventDate = LocalDate.parse("2017-11-18");
 
+		
+		//사용자가 형식의 맞춰 날짜입력을 맞게 할때까지 무한 반복
 		while (isContinue) {
 			try {
 				System.out.println("오늘의 날짜를 입력해 주세요! ex)2017-11-15");
 				String today = scanner.next();
 				startDate = LocalDate.parse(today);
-				eventDate = LocalDate.parse("2017-11-18");
 				isContinue = false;
 			} catch (Exception e) {
+				//날짜를 잘못 입력하였을경우 나오는 메세지
 				System.out.println();
 				System.out.println("날짜 입력형식이 잘못 되었습니다.");
 			}
 		}
-
+		
+		//이벤트에 관련된 객체 생성
 		GoodsEvent goodsEvent = new GoodsEvent(goods, startDate, eventDate);
+		
+		//개월수와 일수를 구하기위한 메소드 적용
 		long remainMonth = ChronoUnit.MONTHS.between(goodsEvent.getStartDate(), goodsEvent.getEndDate());
 		long remainDay = ChronoUnit.DAYS.between(goodsEvent.getStartDate(), goodsEvent.getEndDate());
 		Period period = Period.between(goodsEvent.getStartDate(), goodsEvent.getEndDate());
 
+		//출력
 		System.out.println();
 		System.out.println(goods.getName() + " 프로모션 행사!");
 		System.out.println(goods.getName() + " 정상 가격은 " + goods.getPrice() + " 입니다");
